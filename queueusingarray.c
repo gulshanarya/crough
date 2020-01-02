f8a82784dd95d515ed577955c73ab0379ff20145
gulshanarya <imgulshanarya@gmail.com>
2020-01-02 18:05:47 +0530

    queue using circular array

--- queueusingarray.c
+++ queueusingarray.c
@@ -1,17 +1,18 @@
 #include<stdlib.h>
 #include<stdio.h>
-#define defVal -1
 
 int queue[10];
 int front = -1,rear = -1; //it represents empty queue
 int len = sizeof(queue)/sizeof(int); //length of queue
+//next = (rear+1)%len; //next data in circular array
+//Note: prev pos = (rear+len-1)%len here: adding len have no effects just to be sure pos is +ve when rear = 0
 
 int IsEmpty() {
     if(front == -1&&rear == -1) return 1; else return 0;
 }
 
 int IsFull() {
-    if(rear == (len-1)) return 1; else return 0;
+    if((rear+1)%len == front) return 1; else return 0;
 }
 
 void Insert(int data) {
@@ -22,9 +23,18 @@ void Insert(int data) {
     else if(IsEmpty()) {
         front = rear = 0;
     } else{
-        rear++;
+        rear = (rear+1)%len;
     }
     queue[rear] = data;
+}
+
+int Front() { // 0 represents empty queue
+    if(rear == -1) {
+        printf("Empty queue\n");
+        return 0;
+    }
+
+    return queue[front];
 }
 
 void Dequeue() {
@@ -32,7 +42,7 @@ void Dequeue() {
         printf("UNDERFLOW\n");
     } else if(front == rear) {
         front = rear = -1; //it represents empty queue
-    } else front++;
+    } else front = (front+1)%len;
 }
 
 void Print() {
@@ -40,15 +50,23 @@ void Print() {
         printf("Empty queue");
         return;
     }
-    for(int i = front; i<= rear; i++) printf("%d ", queue[i]);
+
+    int tmp = front;
+    while(tmp != rear) {
+        printf("%d ", queue[tmp]);
+        tmp = (tmp+1)%len;
+    }
+
+    printf("%d ", queue[tmp]);
 }
 
 int main() {
-
     Insert(45);
-    Insert(60);
-    Insert(100);
+
     Dequeue();
+    Insert(110);
+    Insert(120);
     Print();
+    printf("\n%d", Front());
     return 0;
 }
