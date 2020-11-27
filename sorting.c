ce71749b8b3eb6e7e3556d94a6ab2364554806e2
gulshanarya <imgulshanarya@gmail.com>
2020-11-27 12:11:32 +0530

    sorting update

--- sorting.c
+++ sorting.c
@@ -62,7 +62,68 @@ int* insertionSort(int arr[], int n) {
     return arr;
 }
 
+void merge(int L[],int R[], int A[]) {
+    int nL = sizeof(L)/sizeof(int);
+    int nR = sizeof(L)/sizeof(int);
+    int i,j,k;
+    i = j = k = 0;
+    while(i<nL && j<nR) {
+        if(L[i] <= R[j]) {
+            A[k++] = L[i++];
+        } else {
+            A[k++] = R[j++];
+        }
+    }
+    while(i<nL) {
+        A[k++] = L[i++];
+    }
+    while(j<nR) {
+        A[k++] = L[j++];
+    }
+}
 
+int* mergeSort(int arr[]) {
+    int n = sizeof(arr)/sizeof(int);
+    if(n < 2) {
+        return;
+    }
+    int mid = n/2;
+    int left[mid];
+    int right[n-mid];
+    for(int i = 0; i<mid; i++) {
+        left[i] = arr[i];
+    }
+    for(int i = mid; i<n; i++) {
+        right[i-mid] = arr[i];
+    }
+    mergeSort(left);
+    mergeSort(right);
+    merge(left,right,arr);
+}
+
+int partion(int arr[], int start, int end) {
+    int pivot = arr[end];
+    int pIndex = start;
+    for(int i = 0; i<end; i++) {
+        if(arr[i]<=pivot) {
+            int temp = arr[i];
+            arr[i] = arr[pIndex];
+            arr[pIndex] = temp;
+            pIndex++;
+        }
+    }
+    arr[end] = arr[pIndex];
+    arr[pIndex] = pivot;
+    return pIndex;
+}
+v
+oid quickSort(int arr[], int start, int end) {
+    if(start<end) {
+        int pindex = partion(arr,start, end);
+        quickSort(arr,start, pindex-1);
+        quickSort(arr, pindex+1, end);
+    }
+}
 
 int main() {
     int arr[] = {10, 9, 7, 101, 23, 44, 12, 78, 34, 23};
